62895d1c77d9579129ce2950414d0cb58adedf1b
Xiaodi Wang <xiaodi@kth.se>
2021-04-27 22:35:18 +0200

    Implement extracting Tile and link from HTML

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Sockets;
 using System.Text;
@@ -34,9 +35,28 @@ namespace TinyBrowser {
             // Console.WriteLine(data);
             var title = Regex.Match(data, @"\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>",
                 RegexOptions.IgnoreCase).Groups["Title"].Value;
-            Console.WriteLine(title);
-            
+            Console.WriteLine($"Webpage Title: {title}");
+            // var hyperLinks = Regex.Match(data, @"/(?:ht|f)tps?\:\/\/[a-zA-Z0-9\-.]+\.[a-zA-Z]{2,3}(\/\S*)?/g",
+            //     RegexOptions.IgnoreCase).Groups["Title"].Value;
+            Console.WriteLine("Links");
+            foreach(var link in ExtractHyperLinks(data)) Console.WriteLine(link);
+
+            List<string> ExtractHyperLinks(string html) {
+                var charsToRemove = new [] { "\n", "\r"};
+                foreach (var c in charsToRemove)
+                    html = html.Replace(c, string.Empty);
+                
+                List<string> list = new List<string>();
+
+                var regex = new Regex("<a href=[\"|']([\\w].*?)[\"|'].*?([\\w|\\s|\\x2E]*?)</a>", RegexOptions.None);
+                if(regex.IsMatch(html))
+                    foreach(Match match in regex.Matches(html))
+                        list.Add(match.Groups[2].Value.TrimStart(' ').TrimEnd(' ').PadRight(21) + "   " + match.Groups[1].Value);
+
+                return list;
+            }
         }
+        
     }
 
 }
