6a5a428c6d1402e106275bce00b0527c3e7c5292
Xiaodi Wang <xiaodi@kth.se>
2021-04-28 13:33:59 +0200

    Implement "b""f""r""h" functions

--- TinyBrowser/Program.cs
+++ TinyBrowser/Program.cs
@@ -8,73 +8,129 @@ using System.Text.RegularExpressions;
 namespace TinyBrowser {
 
     internal static class Program {
+        const string Hostname = "acme.com";
+        const int Port = 80;
+        static string version = "1.1";
         private static void Main(string[] args) {
-            using var client = new TcpClient();
+            var exit = false;
+            var localLink = string.Empty;
+            var history = new List<string>();
+            var historyPointer = 0;
+            var newPage = true;
+            var printResult = true;
+            while(!exit) {
+                if(newPage) {
+                    history.Add(localLink);
+                    historyPointer = history.Count-1;
+                }
+                var targetLink = history[historyPointer];
+                using var client = new TcpClient();
+                client.Connect(Hostname, Port);
+                using var networkStream = client.GetStream();
+                networkStream.ReadTimeout = 2000;
+                using var writer = new StreamWriter(networkStream);
+                string requestMeg;
+                if(targetLink.StartsWith("//") | targetLink.StartsWith("http"))
+                    requestMeg = RequestLine(version, targetLink);
+                else requestMeg = RequestLine(version, Hostname, targetLink);
 
-            const string hostname = "acme.com";
-            const int port = 80;
-            string version = "1.1";
-            client.Connect(hostname, port);
-
-            using var networkStream = client.GetStream();
-            networkStream.ReadTimeout = 2000;
-
-            using var writer = new StreamWriter(networkStream);
-
-            var requestMeg = $"GET /";
-            switch(version) {
-                case "0.9": requestMeg += "\r\n";
-                    break;
-                case "1.1":
-                    requestMeg += $" HTTP/{version}" 
-                                  + "\r\nAccept: text/html, charset=utf-8" 
-                                  + "\r\nAccept-Language: en-US" 
-                                  + "\r\nUser-Agent: C# program" 
-                                  + "\r\nConnection: close" 
-                                  + $"\r\nHost: {hostname}" + "\r\n\r\n";
-                    break;
-
-            }
-
-            var bytes = Encoding.UTF8.GetBytes(requestMeg);
-            networkStream.Write(bytes, 0, bytes.Length);
-            using var reader = new StreamReader(networkStream, Encoding.UTF8);
-            var data = reader.ReadToEnd();
-            // Console.WriteLine(data);
-            var title = Regex.Match(data, "<title>(?<Title>.*?)</title>",
-                RegexOptions.IgnoreCase).Groups["Title"].Value;
-            Console.WriteLine($"Webpage Title: {title}");
-
-            Console.WriteLine("Links");
-            var links = ExtractHyperLinks(data);
-            for(int i = 0; i < links.Count; i++) {
-                Console.WriteLine($"{(i + ":").PadRight(3)} {links[i][0].TrimStart().TrimEnd()} ({links[i][1]})");
-            }
-            
-            List<string[]> ExtractHyperLinks(string html) {
-                //Remove all new Lines and carriage return characters
-                var charsToRemove = new [] { "\n", "\r"};
-                foreach (var c in charsToRemove)
-                    html = html.Replace(c, string.Empty);
-                
-                List<string[]> list = new ();
-                //[\"|'] => a single character is " or '
-                //() Select in () as indexed Group, or give name to each group (?<GroupName> exp);
-                //(?<link>.*?) Named Group
-                //.*? => any character (without \n) 0 or more times (Lazy mode), 
-                //[\\w|\\s|\\x2E]*? => accept word or space or "." 0 or more times. \\x2E = ASCII '.'
-                // \\s* ignore 0 or more white-space
-                //(<b>|<img.*?>)? ignore <b> or <img .... > 0 or 1 time;
-                //(</b>)? ignore </b> 0 or 1 time;
-                var regex = new Regex("<a href=[\"|'][/]*(?<link>.*?)[\"|'].*?>(<b>|<img.*?>)?(?<name>.*?)(</b>)?</a>", 
-                    RegexOptions.None);
-                if(regex.IsMatch(html))
-                    foreach(Match match in regex.Matches(html))
-                        list.Add(new []{match.Groups["name"].Value, match.Groups["link"].Value});
-                return list;
+                var bytes = Encoding.UTF8.GetBytes(requestMeg);
+                networkStream.Write(bytes, 0, bytes.Length);
+                using var reader = new StreamReader(networkStream, Encoding.UTF8);
+                var data = reader.ReadToEnd();
+                // Console.WriteLine(data);
+                client.Close();
+                var title = ExtractHeadInfo(data, "Title");
+                if(printResult)Console.WriteLine($"Webpage Title: {title}");
+                if(printResult)Console.WriteLine("Links");
+                var links = ExtractHyperLinks(data);
+                for(int i = 0; i < links.Count; i++) 
+                    if(printResult)Console.WriteLine($"{(i + ":").PadRight(3)} {links[i][0].TrimStart().TrimEnd()} ({links[i][1]})");
+                Console.WriteLine("Type in 'q' to exit!");
+                var s = Console.ReadLine();
+                int linksIndex;
+                bool isNumber = int.TryParse(s, out linksIndex);
+                newPage = isNumber;
+                printResult = !Equals(s, "h");
+                switch(s) {
+                    default:
+                        if(isNumber) localLink = links[linksIndex][1];
+                        break;
+                    case "h":
+                        for(var i = 0; i < history.Count; i++) {
+                            var pointer = historyPointer == i ? "==>" : "   ";
+                            Console.WriteLine($"{pointer} History({i}): {history[i]}");
+                        }
+                        break;
+                    case "b":
+                        if(historyPointer > 0) historyPointer--;
+                        break;
+                    case "f":
+                        if(history.Count - 1 > historyPointer) historyPointer++;
+                        break;
+                    case "r": break;
+                }
+                exit = Equals(s, "q") | Equals(s, "Q");
             }
         }
-        
+
+        static List<string[]> ExtractHyperLinks(string html) {
+            //Remove all new Lines and carriage return characters
+            var charsToRemove = new [] { "\n", "\r"};
+            foreach (var c in charsToRemove)
+                html = html.Replace(c, string.Empty);
+                
+            List<string[]> list = new ();
+            //[\"|'] => a single character is " or '
+            //() Select in () as indexed Group, or give name to each group (?<GroupName> exp);
+            //(?<link>.*?) Named Group
+            //.*? => any character (without \n) 0 or more times (Lazy mode), 
+            //[\\w|\\s|\\x2E]*? => accept word or space or "." 0 or more times. \\x2E = ASCII '.'
+            // \\s* ignore 0 or more white-space
+            //(<b>|<img.*?>)? ignore <b> or <img .... > 0 or 1 time;
+            //(</b>)? ignore </b> 0 or 1 time;
+            var regex = new Regex("<a href=[\"|'](?<link>.*?)[\"|'].*?>(<b>|<img.*?>)?(?<name>.*?)(</b>)?</a>", 
+                RegexOptions.None);
+            if(regex.IsMatch(html))
+                foreach(Match match in regex.Matches(html))
+                    list.Add(new []{match.Groups["name"].Value, match.Groups["link"].Value});
+            return list;
+        }
+
+        private static string ExtractHeadInfo(string data, string info) {
+            return Regex.Match(data, $"<{info}>(?<Info>.*?)</{info}>", RegexOptions.IgnoreCase).Groups["Info"].Value;
+        }
+
+        private static string RequestLine(string v, string host, string localLink) {
+            var requestMeg = $"GET /{localLink}";
+            switch(v) {
+                case "0.9":
+                    requestMeg += "\r\n";
+                    break;
+                case "1.1":
+                    requestMeg += $" HTTP/{v}" + 
+                                  "\r\nAccept: text/html, charset=utf-8" + 
+                                  "\r\nAccept-Language: en-US" +
+                                  "\r\nUser-Agent: C# program" + 
+                                  "\r\nConnection: close" + 
+                                  $"\r\nHost: {host}" + "\r\n\r\n";
+                    break;
+            }
+            return requestMeg;
+        }
+        private static string RequestLine(string v, string localLink) {
+            var requestMeg = $"GET {localLink}";
+            switch(v) {
+                case "0.9":
+                    requestMeg += "\r\n";
+                    break;
+                case "1.1":
+                    requestMeg += $" HTTP/{v}" + 
+                                  "\r\n\r\n";
+                    break;
+            }
+            return requestMeg;
+        }
     }
 
 }
