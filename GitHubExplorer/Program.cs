0d53d4d3cf34b142c449aaa1426b6f2955c11aa9
Xiaodi Wang <xiaodi@kth.se>
2021-04-29 15:58:08 +0200

    Implement get user information from api.github.com/users/XiaodiW and Json convert to class type

--- GitHubExplorer/Program.cs
+++ GitHubExplorer/Program.cs
@@ -7,29 +7,33 @@ using System.Threading.Tasks;
 
 namespace GitHubExplorer
 {
-    class Program
-    {
+    class Program {
         private static readonly HttpClient Client = new ();
+        static readonly string _userName = "XiaodiW";
+        static readonly string _token = "ghp_1QTag";
         static async Task Main(string[] args){
             Client.DefaultRequestHeaders.Accept.Clear();
             Client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
-            Client.DefaultRequestHeaders.UserAgent.TryParseAdd("request");
+            Client.DefaultRequestHeaders.UserAgent.TryParseAdd(_userName);
             var requestMeg = new HttpRequestMessage();
-            requestMeg.RequestUri = new Uri("https://api.github.com/users/XiaodiW");
+            requestMeg.RequestUri = new Uri($"https://api.github.com/users/{_userName}");
             requestMeg.Method = HttpMethod.Get;
-            requestMeg.Headers.Authorization = AuthenticationHeaderValue.Parse("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");
+            requestMeg.Headers.Authorization = AuthenticationHeaderValue.Parse(_token);
             var received = Client.SendAsync(requestMeg);
             Console.WriteLine(await received);
             var stream = await received.Result.Content.ReadAsStreamAsync();
             var reader = new StreamReader(stream);
             var data = await reader.ReadToEndAsync();
-            Console.WriteLine(data);
+            // Console.WriteLine(data);
             var userInfo = JsonSerializer.Deserialize<UserResponse>(data);
             Console.WriteLine($"Name: {userInfo.name}");
             Console.WriteLine($"Link: {userInfo.html_url}");
+            Console.WriteLine($"Repos: {userInfo.repos_url}");
             Console.WriteLine($"Created at: {userInfo.created_at}");
             Client.Dispose();
         }
+
+
     }
 
     public class UserResponse {
@@ -65,4 +69,27 @@ namespace GitHubExplorer
         public string created_at{ get; set; }
         public string updated_at{ get; set; }
     }
+
+    /*public class Secrets {
+        public string Token { get; set; }
+        private static readonly string FileName = Path.Combine(Environment.CurrentDirectory, "secrets.json");
+
+        public Secrets(string token) {
+            Token = token;
+        }
+
+        private static Secrets LoadAndValidateSectets() {
+            Secrets secrets;
+            if(!File.Exists(FileName)) {
+                var token = Console.ReadLine();
+                secrets = new Secrets(token);
+                File.WriteAllText(FileName, JsonSerializer.Serialize(secrets));
+            } else { secrets = JsonSerializer.Deserialize<Secrets>(File.ReadAllText(FileName)); }
+            if(string.IsNullOrEmpty(secrets.Token)) {
+                Console.WriteLine("Error:");
+                throw new Exception($"Error: Need to define a Token in file {FileName}.");
+            }
+            return secrets;
+        }
+    }*/
 }
